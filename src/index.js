7c597d817d86cdd51b01b49d38837a757d8adb7a
Artem Berdyshev <berdartem@gmail.com>
2018-07-16 22:43:44 +0300

    install create-react-app and remove all boilerplate

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => <div>Widget</div>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
