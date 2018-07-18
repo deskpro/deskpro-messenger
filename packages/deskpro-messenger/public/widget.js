354b6bd4abbf8040ebd5192629250ebe1db3afa8
Artem Berdyshev <berdartem@gmail.com>
2018-07-19 00:23:27 +0300

    improve widget loader build script

--- packages/deskpro-messenger/public/widget.js
+++ packages/deskpro-messenger/public/widget.js
@@ -22,24 +22,10 @@
     document.body.appendChild(container);
 
     var iframeDoc = iframe.contentDocument;
-    var cssLink =
-      '<link rel="stylesheet" href="' +
-      options.widgetUrl +
-      '/static/css/main.css">';
-    // var cssModulesLink =
-    //   '<link rel="stylesheet" href="' +
-    //   options.widgetUrl +
-    //   '/static/css/main-cssmodules.css">';
-    var scriptTag =
-      '<script async src="' +
-      options.widgetUrl +
-      '/static/js/bundle.js"></script>';
+
     var initialContent =
-      '<!DOCTYPE html><html><head>' +
-      cssLink +
-      // cssModulesLink +
-      scriptTag +
-      '</head><body></body></html>';
+      '<!DOCTYPE html><html><head><!-- INJECT ASSETS --></head><body></body></html>';
+    initialContent = initialContent.replace(/{HOST}/g, options.widgetUrl);
     iframeDoc.open('text/html', 'replace');
     iframeDoc.write(initialContent);
     iframeDoc.close();
