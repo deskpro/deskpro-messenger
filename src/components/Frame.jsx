c2e315c45b6ced642d75484066d9f31e3dfc9db8
Artem Berdyshev <berdartem@gmail.com>
2018-07-18 23:31:27 +0300

    make the app work in local (dev) environment

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import FrameComponent from 'react-frame-component';
 
-const loaderIframe = window.parent.document.getElementById(
-  'deskpro-widget-loader'
-);
-const styleLink = loaderIframe
-  ? loaderIframe.contentDocument.head.querySelector('link[rel="stylesheet"]')
-      .href
-  : null;
+let head;
+
+// in production env this the styles could be extracted only once when the app
+// is starting and then this styles could be loaded inside each frame.
+if (process.env.NODE_ENV === 'production') {
+  const loaderIframe = window.parent.document.getElementById(
+    'deskpro-widget-loader'
+  );
+  if (loaderIframe) {
+    const styleLink = loaderIframe.contentDocument.head.querySelector(
+      'link[rel="stylesheet"]'
+    ).href;
+    head = <link rel="stylesheet" href={styleLink} />;
+  }
+}
 
 const defaultIframeStyle = {
   position: 'fixed',
@@ -18,9 +26,19 @@ const defaultIframeStyle = {
 };
 
 const Frame = ({ children, style = {}, ...props }) => {
+  // in development mode styles are injected on the fly with HMR, so we need to
+  // extract them on component rendering.
+  if (process.env.NODE_ENV === 'development') {
+    // pull css from all style tags because we might not know which one is
+    // generated and inserted by webpack HMR.
+    const style = Array.from(document.head.querySelectorAll('style'))
+      .map(el => el.innerText)
+      .join('\\n');
+    head = <style type="text/css">{style}</style>;
+  }
   return (
     <FrameComponent
-      head={!!styleLink && <link rel="stylesheet" href={styleLink} />}
+      head={head}
       frameBorder="0"
       scrolling="no"
       style={{ ...defaultIframeStyle, ...style }}
