aa24287ceef7fb2b396a245f1c237f57ecad2d87
Artem Berdyshev <berdartem@gmail.com>
2018-08-01 23:50:47 +0300

    add dummy tickets screen

---
+++ src/components/screens/TicketsScreen.jsx
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+const TicketsScreen = () => (
+  <Fragment>
+    <h1>Your Tickets</h1>
+    <ul>
+      <li>Ticket #1</li>
+      <li>Ticket #2</li>
+      <li>Ticket #3</li>
+      <li>Ticket #4</li>
+      <li>Ticket #5</li>
+    </ul>
+  </Fragment>
+);
+
+export default TicketsScreen;
