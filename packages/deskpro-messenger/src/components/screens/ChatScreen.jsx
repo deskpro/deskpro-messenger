3ae4dfb2cdfd24d278d4bebbf07b2c656adaf576
Artem Berdyshev <berdartem@gmail.com>
2018-08-01 23:50:20 +0300

    implement screen for conversations with fake chat functionality

---
+++ packages/deskpro-messenger/src/components/screens/ChatScreen.jsx
@@ -0,0 +1,37 @@
+import React, { Fragment, PureComponent } from 'react';
+
+class ChatScreen extends PureComponent {
+  state = { messages: [], newMessageText: '' };
+
+  handleMessageChange = e => this.setState({ newMessageText: e.target.value });
+
+  handleSendMessage = e => {
+    e.preventDefault();
+    if (this.state.newMessageText) {
+      this.setState({
+        messages: this.state.messages.concat([this.state.newMessageText]),
+        newMessageText: ''
+      });
+    }
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <h1>{this.props.match.params.category} chat</h1>
+        {this.state.messages.map((message, idx) => <p key={idx}>{message}</p>)}
+        <textarea
+          rows={2}
+          name="message"
+          id="message"
+          placeholder="Enter your message"
+          value={this.state.newMessageText}
+          onChange={this.handleMessageChange}
+        />
+        <button onClick={this.handleSendMessage}>Send</button>
+      </Fragment>
+    );
+  }
+}
+
+export default ChatScreen;
