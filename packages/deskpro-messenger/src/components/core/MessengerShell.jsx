0e9c4d22e154da7c6d2a3f8d8a69287d946b9b1b
Artem Berdyshev <berdartem@gmail.com>
2018-08-07 00:33:48 +0300

    hardcode styles for now; apply status for toggler and messenger shell

---
+++ packages/deskpro-messenger/src/components/core/MessengerShell.jsx
@@ -0,0 +1,75 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+class MessengerShell extends PureComponent {
+  static propTypes = {
+    isMinimal: PropTypes.bool,
+    theme: PropTypes.oneOf(['blue', 'light']),
+    title: PropTypes.string,
+    introText: PropTypes.string,
+    children: PropTypes.any,
+    controls: PropTypes.any
+  };
+
+  static defaultProps = {
+    isMinimal: true,
+    theme: 'blue',
+    title: 'Get in touch',
+    introText: ''
+  };
+
+  render() {
+    const {
+      isMinimal,
+      theme,
+      title,
+      introText,
+      children,
+      controls
+    } = this.props;
+
+    return (
+      <div
+        className="dpmsg-ScreenWrap"
+        style={{ marginLeft: '5px', marginTop: '5px' }}
+      >
+        <div
+          className={classNames('dpmsg-Screen', {
+            'is-minimal': isMinimal,
+            'is-blue': theme === 'blue',
+            'is-light': theme === 'light'
+          })}
+        >
+          <div className="dpmsg-ScreenHeder">
+            {controls}
+            <div className="dpmsg-ScreenHederLogo">
+              <img
+                src="https://deskpro.github.io/messenger-style/img/dp-logo-white.svg"
+                alt=""
+              />
+            </div>
+            <span className="dpmsg-ScreenHederTitle">{title}</span>
+            {!!introText && (
+              <span className="dpmsg-ScreenHederText">{introText}</span>
+            )}
+          </div>
+          {children}
+          <div className="dpmsg-ScreenFooter">
+            <span className="dpmsg-ScreenLine" />
+            <span className="dpmsg-ScreenFooterText">Powered by</span>
+            <span className="dpmsg-VertLine" />
+            <img
+              className="dpmsg-ScreenFooterLogo"
+              src="https://deskpro.github.io/messenger-style/img/deskpro-logo.svg"
+              alt=""
+            />
+            <span className="dpmsg-ScreenLine" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MessengerShell;
--- packages/deskpro-messenger/src/components/core/MessengerToggler.jsx
+++ packages/deskpro-messenger/src/components/core/MessengerToggler.jsx
@@ -20,17 +20,16 @@ const iframeStyle = {
   position: 'fixed',
   right: '14px',
   bottom: '14px',
-  width: '160px',
-  height: '40px'
+  width: '70px',
+  height: '70px'
 };
 
 class WidgetToggler extends PureComponent {
   static propTypes = {
     location: PropTypes.shape({
-      pathname: PropTypes.string.isRequired,
-    }).isRequired,
+      pathname: PropTypes.string.isRequired
+    }).isRequired
   };
-
 
   render() {
     const opened = this.props.location.pathname.startsWith('/screens');
@@ -38,13 +37,23 @@ class WidgetToggler extends PureComponent {
     return (
       <Frame style={iframeStyle}>
         <Link
-          className={classNames(`widget-toggler--button`, {
-            'widget-toggler--button__opened': opened,
-            'widget-toggler--button__collapsed': !opened
+          className={classNames(`dpmsg-TriggerBtn`, {
+            'dpmsg-Icon dpmsg-IconClose': opened
+            // 'widget-toggler--button__collapsed': !opened
           })}
           to={opened ? '/' : '/screens'}
         >
-          {opened ? 'Close' : 'Click Me!'}
+          {opened ? (
+            <img
+              src="https://deskpro.github.io/messenger-style/img/docs/close-icon.png"
+              alt=""
+            />
+          ) : (
+            <img
+              src="https://deskpro.github.io/messenger-style/img/docs/avatar.png"
+              alt=""
+            />
+          )}
         </Link>
       </Frame>
     );
