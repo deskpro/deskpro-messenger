3b51b58e21ba14dd13ac970fcf5bf6a6c8c5337d
Artem Berdyshev <berdartem@gmail.com>
2018-11-30 12:51:43 +0200

    cover SaveTicketBlock with tests

--- packages/deskpro-messenger/src/setupTests.js
+++ packages/deskpro-messenger/src/setupTests.js
@@ -5,4 +5,16 @@ require('jest-dom/extend-expect');
 // this is basically: afterEach(cleanup)
 require('react-testing-library/cleanup-after-each');
 
+// skip missing translation strings warnings.
+const consoleError = console.error.bind(console);
+console.error = (message, ...args) => {
+  if (
+    typeof message === 'string' &&
+    message.startsWith('[React Intl] Missing message:')
+  ) {
+    return;
+  }
+  consoleError(message, ...args);
+};
+
 jest.setTimeout(30000);
