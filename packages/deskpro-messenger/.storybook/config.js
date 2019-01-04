0b28f31c30f516e0ae0ecc2b937663b70a9942eb
Artem Berdyshev <berdartem@gmail.com>
2019-01-05 01:57:50 +0200

    copy styles into stroybook-static

--- packages/deskpro-messenger/.storybook/config.js
+++ packages/deskpro-messenger/.storybook/config.js
@@ -3,8 +3,8 @@ import { configure } from '@storybook/react';
 window.parent.DESKPRO_MESSENGER_OPTIONS = {
   baseUrl:
     process.env.NODE_ENV === 'development'
-      ? 'http://localhost:9009/'
-      : 'https://deskpro.github.io/deskpro-messenger/'
+      ? 'http://localhost:9009/assets/'
+      : 'https://deskpro.github.io/deskpro-messenger/assets/'
 };
 
 function loadStories() {
