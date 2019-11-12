886c73daed655f274e9f028fb9a5d15d637a65c6
ImmortaL <denis@ranneft.ru>
2019-11-12 13:48:25 +0300

    fix aseets log

--- packages/deskpro-messenger/src/utils/asset.js
+++ packages/deskpro-messenger/src/utils/asset.js
@@ -1,6 +1,6 @@
 window.parent.DESKPRO_MESSENGER_OPTIONS =
   window.parent.DESKPRO_MESSENGER_OPTIONS || {};
-console.log(window.parent.DESKPRO_MESSENGER_OPTIONS);
+
 export default (path) => {
   return `${window.parent.DESKPRO_MESSENGER_OPTIONS.baseUrl}/assets/${path}`;
 };
