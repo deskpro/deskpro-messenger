9ae3e4c165c57c76c165d88b00d176591b7ecbfe
ImmortaL <denis@ranneft.ru>
2020-01-03 14:55:08 +0200

    [ch1723] fix rating

--- packages/deskpro-messenger/src/components/chat/RatingBlock.jsx
+++ packages/deskpro-messenger/src/components/chat/RatingBlock.jsx
@@ -43,7 +43,7 @@ export class RatingBlock extends PureComponent {
       this.props.onSend({
         type: 'chat.rating',
         origin: 'user',
-        rate: 0 + (rating === 'helpful')
+        rate: rating === 'helpful'
       });
     });
   };
