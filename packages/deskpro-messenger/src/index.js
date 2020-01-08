6b1edf636e3867b2ff4eb2bd6cb9332095a97934
ImmortaL <denis@ranneft.ru>
2020-01-08 21:28:40 +0200

    scroll lock when operating inside messenger widget

--- packages/deskpro-messenger/src/index.js
+++ packages/deskpro-messenger/src/index.js
@@ -1,10 +1,15 @@
 import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+
+const container = window.parent.document.getElementById('deskpro-container');
+container.addEventListener('mouseenter', (e) => disableBodyScroll(container));
+container.addEventListener('mouseleave', (e) => enableBodyScroll(container));
+
 
 ReactDOM.render(
   <App />,
-  window.parent.document.getElementById('deskpro-container')
+  container
 );
