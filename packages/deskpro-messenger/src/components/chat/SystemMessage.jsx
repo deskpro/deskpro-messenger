a5b623a210917f8aa93d661ef92057a23c43e14a
ImmortaL <denis@ranneft.ru>
2020-01-21 23:53:09 +0200

    timestamps on chat events

--- packages/deskpro-messenger/src/components/chat/SystemMessage.jsx
+++ packages/deskpro-messenger/src/components/chat/SystemMessage.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import moment from 'moment';
 
-const SystemMessage = ({ message, origin }) => (
+const SystemMessage = ({ date_created, message, origin }) => (
   <span
     className={classNames('dpmsg-BubbleNotification', {
       'is-joined': origin === 'system',
       'is-end': origin !== 'system'
     })}
   >
-    {message}
+    {message} ({moment(date_created).format('hh:mm a')})
   </span>
 );
 
