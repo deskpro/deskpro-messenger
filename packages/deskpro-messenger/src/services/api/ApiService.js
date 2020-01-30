4cdc09f904a78a6a8456024ea96b6665321f74b1
ImmortaL <denis@ranneft.ru>
2020-01-30 02:53:16 +0200

    [ch4819] fix attachments

--- packages/deskpro-messenger/src/services/api/ApiService.js
+++ packages/deskpro-messenger/src/services/api/ApiService.js
@@ -150,6 +150,7 @@ export default class ApiService {
    * @param {object} values Ticket values.
    */
   async createTicket(values) {
+    values.attachments = values.attachments.map(a => ({ blob_auth: a.authcode }));
     return this.apiClient.post(`/api/messenger/ticket`, values);
   }
 
