9cdb4f153446dad45ecf10d6a0804f4db646d67a
ImmortaL <denis@ranneft.ru>
2020-03-02 12:47:17 +0300

    remove double footer from pro-active chat

--- packages/deskpro-messenger/src/components/core/AutoStartShell.jsx
+++ packages/deskpro-messenger/src/components/core/AutoStartShell.jsx
@@ -62,19 +62,6 @@ class AutoStartShell extends PureComponent {
           <ScreenContent>
             {children}
           </ScreenContent>
-          <div className="dpmsg-ScreenFooter">
-            <span className="dpmsg-ScreenLine" />
-            <span className="dpmsg-ScreenFooterText">Powered by</span>
-            <span className="dpmsg-VertLine" />
-            <a href="https://deskpro.com" target="_blank" rel="noreferrer noopener" title="Deskpro">
-              <img
-                className="dpmsg-ScreenFooterLogo"
-                src={asset('img/deskpro-logo.svg')}
-                alt="Deskpro"
-              />
-            </a>
-            <span className="dpmsg-ScreenLine" />
-          </div>
         </div>
       </div>
     );
