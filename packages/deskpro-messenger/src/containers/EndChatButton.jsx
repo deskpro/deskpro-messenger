204dede8a52d1731f24c625f78a58e4fff52ca8f
ImmortaL <denis@ranneft.ru>
2020-03-03 12:56:21 +0300

    [ch9777]

--- packages/deskpro-messenger/src/containers/EndChatButton.jsx
+++ packages/deskpro-messenger/src/containers/EndChatButton.jsx
@@ -1,12 +1,24 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { toggleChatEndBlock} from '../modules/chat'
 
 class EndChatButton extends PureComponent
 {
+
+  static propTypes = {
+    toggleChatEndBlock: PropTypes.func.isRequired
+  };
+
   render() {
-    return <span onClick={() => this.props.toggleChatEndBlock(true)}>End chat</span>
+    return (
+      <div className="dpmsg-endChatButton">
+        <span onClick={() => this.props.toggleChatEndBlock(true)}>End chat</span>
+      </div>
+    );
   }
 }
 
-export default connect(null, { toggleChatEndBlock})(EndChatButton);
+
+
+export default connect(null, { toggleChatEndBlock })(EndChatButton);
