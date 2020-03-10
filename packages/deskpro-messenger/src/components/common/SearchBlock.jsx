fc426e34c444dfb58c1b6429a81e561aaa35b168
ImmortaL <denis@ranneft.ru>
2020-03-10 20:01:11 +0300

    remove unused import

--- packages/deskpro-messenger/src/components/common/SearchBlock.jsx
+++ packages/deskpro-messenger/src/components/common/SearchBlock.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
-import asset from '../../utils/asset';
 
 class SearchBlock extends React.Component {
   static propTypes = {
