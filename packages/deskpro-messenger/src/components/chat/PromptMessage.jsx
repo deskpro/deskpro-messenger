ebcafb647aa03e8fa3e481a80c61612b15e83274
ImmortaL <denis@ranneft.ru>
2020-03-12 19:01:34 +0300

    remove unused

--- packages/deskpro-messenger/src/components/chat/PromptMessage.jsx
+++ packages/deskpro-messenger/src/components/chat/PromptMessage.jsx
@@ -2,26 +2,17 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom'
 import { withFrameContext } from '../core/Frame';
-
-import MessageForm from './MessageForm';
 import { withConfig } from '../core/ConfigContext';
 import BotBubble from './BotBubble';
 import { compose } from 'redux';
 import { withScreenContentSize } from '../core/ScreenContent';
 import { connect } from 'react-redux';
 import { isMessageFormFocused } from '../../modules/app';
-import isMobile from 'is-mobile';
-
-const mobile = isMobile();
 
 class PromptMessage extends PureComponent {
   static propTypes = {
     prompt: PropTypes.string,
     onSendMessage: PropTypes.func.isRequired
-  };
-
-  handleEndChat = () => {
-    this.props.history.goBack();
   };
 
   render() {
