74c3c31d2c91a7c9f583984569a814615905c233
ImmortaL <denis@ranneft.ru>
2020-03-17 21:24:51 +0300

    fix assets double slash

--- packages/deskpro-messenger/src/utils/asset.js
+++ packages/deskpro-messenger/src/utils/asset.js
@@ -2,5 +2,6 @@ window.parent.DESKPRO_MESSENGER_OPTIONS =
   window.parent.DESKPRO_MESSENGER_OPTIONS || {};
 
 export default (path) => {
-  return `${window.parent.DESKPRO_MESSENGER_OPTIONS.baseUrl}/assets/${path}`;
+  const edited = window.parent.DESKPRO_MESSENGER_OPTIONS.baseUrl.replace(/\/$/, "");
+  return `${edited}/assets/${path}`;
 };
