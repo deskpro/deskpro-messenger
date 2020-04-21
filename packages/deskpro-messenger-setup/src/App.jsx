6f06a66764c7a1830e0fc7b5e1bdcc62f73b37af
ImmortaL <denis@ranneft.ru>
2020-04-21 14:06:08 +0300

    [ch11937] remove preview for now

--- packages/deskpro-messenger-setup/src/App.jsx
+++ packages/deskpro-messenger-setup/src/App.jsx
@@ -44,7 +44,6 @@ class App extends React.Component {
             { this.props.children }
           </div>
         </Settings>
-        <Preview config={transformConfig(settings)}/>
       </div>
     );
   }
